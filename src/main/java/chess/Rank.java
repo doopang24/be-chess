a2a8b4977fd813b858ad6897f148d5c770e39bc5
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-10 16:33:58 +0900

    feat: Rank 클래스 구현
    Rank 클래스를 일급컬렉션으로 구현

---
+++ src/main/java/chess/Rank.java
@@ -0,0 +1,25 @@
+package chess;
+
+import pieces.Piece;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rank {
+
+    private final List<Piece> RANK = new ArrayList<>();
+    private final int RANK_SIZE = 8;
+
+    public void addRank(Piece piece) {
+        RANK.add(piece);
+    }
+
+    public String getRankView() {
+        StringBuilder builder = new StringBuilder();
+        for (Piece piece : RANK) {
+            builder.append(piece.getRepresentation());
+        }
+        return builder.toString();
+    }
+
+}
