2e8cf13b44b06ee5465d154773e2449f5bee7063
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-10 20:22:03 +0900

    feat: BoardTest 수정

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -2,9 +2,11 @@ package chess;
 
 import static utils.StringUtils.appendNewLine;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import pieces.Piece;
 
 public class BoardTest {
     private Board board;
@@ -18,7 +20,7 @@ public class BoardTest {
     @Test
     public void create() throws Exception {
         board.initialize();
-        assertEquals(32, board.pieceCount());
+        assertEquals(32, board.getInitialPieceTotalCount());
         String blackRank = appendNewLine("........");
         assertEquals(
                 appendNewLine("RNBQKBNR") +
@@ -27,5 +29,27 @@ public class BoardTest {
                         appendNewLine("pppppppp") +
                         appendNewLine("rnbqkbnr"),
                 board.showBoard());
+    }
+
+    @Test
+    public void findPiece() throws Exception {
+        board.initialize();
+
+        assertThat(Piece.createBlackRook()).isEqualTo(board.findPiece("a8"));
+        assertThat(Piece.createBlackRook()).isEqualTo(board.findPiece("h8"));
+        assertThat(Piece.createWhiteRook()).isEqualTo(board.findPiece("a1"));
+        assertThat(Piece.createWhiteRook()).isEqualTo(board.findPiece("h1"));
+    }
+
+    @Test
+    public void move() throws Exception {
+        board.initializeEmpty();
+
+        String position = "b5";
+        Piece piece = Piece.createBlackRook();
+        board.move(position, piece);
+
+        assertThat(piece).isEqualTo(board.findPiece(position));
+        System.out.println(board.showBoard());
     }
 }
