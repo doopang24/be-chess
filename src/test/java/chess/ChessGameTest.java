1724944dd72d1e2fef7ffd28ea681000d83ac65e
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-12 21:33:05 +0900

    feat: ChessGameTest 클래스 생성
    ChessGame의 move() 테스트 구현

---
+++ src/test/java/chess/ChessGameTest.java
@@ -0,0 +1,29 @@
+package chess;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.within;
+
+import org.assertj.core.api.GenericComparableAssert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pieces.Piece;
+
+public class ChessGameTest {
+
+    @Test
+    @DisplayName("")
+    public void move() throws Exception {
+        Board board = new Board();
+        board.initialize();
+
+        ChessGame game = new ChessGame(Board.BOARD);
+
+        String sourcePosition = "b2";
+        String targetPosition = "b3";
+        game.move(sourcePosition, targetPosition);
+        assertThat(Piece.createBlank(Position.createPos(sourcePosition))).isEqualTo(game.findPiece(sourcePosition));
+        assertThat(Piece.createWhitePawn(Position.createPos(targetPosition))).isEqualTo(game.findPiece(targetPosition));
+    }
+}
