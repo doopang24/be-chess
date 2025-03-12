699d61a5ccde4f68f5524e02cc2932a25cc90200
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-12 11:36:01 +0900

    feat: main()에 move 추가
    사용자가 move 명령어를 사용할 수 있도록 로직 추가

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -9,21 +9,24 @@ public class Application {
     public String getInput() {
         while (true) {
             String input = scanner.nextLine();
-            if (input.equalsIgnoreCase("start") || input.equalsIgnoreCase("end")) {
+            if (input.equalsIgnoreCase("start") || input.equalsIgnoreCase("end") || input.startsWith("move")) {
                 return input;
             } else {
-                System.out.println("start 와 end 중 하나를 입력하세요.");
+                System.out.println("유효한 명령어를 입력하세요.");
             }
         }
     }
 
     public static void main(String[] args) {
         Application application = new Application();
+        ChessGame game = new ChessGame();
         Board board = new Board();
 
         String input = application.getInput();
         if (input.equalsIgnoreCase("start")) {
             board.initialize();
+        } else if (input.startsWith("move")) {
+            game.move(input);
         } else {
             System.out.println("게임을 종료합니다.");
         }
