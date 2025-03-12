445437c58994dee05a39e1fc1d10afe4dd51a13c
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-12 21:36:02 +0900

    refactor: Board 클래스 수정
    Board 클래스는 초기화를 담당하도록 수정

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,19 +1,17 @@
 package chess;
 
-import static utils.StringUtils.appendNewLine;
-
 import pieces.Piece;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Board {
 
-    LocationConverter converter = new LocationConverter();
+    ChessView view = new ChessView();
 
-    private final List<Rank> BOARD = new ArrayList<>();
+    public static final String FILE = "abcdefgh";
+    public static final String RANK = "12345678";
+
+    public static final List<Rank> BOARD = new ArrayList<>();
 
     private final int FILE_SIZE = 8;
     private final int RANK_SIZE = 8;
@@ -58,7 +56,7 @@ public class Board {
         BOARD.add(whitePawn);
         BOARD.add(whitePiece);
 
-        System.out.println(showBoard());
+        view.printBoard(BOARD);
     }
 
     private void makeBlackPieceRank() {
@@ -129,92 +127,59 @@ public class Board {
 
     public void initializeEmpty() {
         makeEmptyRank(RANK_SIZE);
+        view.printBoard(BOARD);
     }
 
-    // 지정한 위치에 기물 배치
-    public void move(String position, Piece piece) {
-        try {
-            isValidNotation(position);
-            int fileIndex = converter.fileToIndex(position.charAt(0));
-            int rankIndex = converter.rankToIndex(position.charAt(1));
-            BOARD.get(rankIndex).putPiece(fileIndex, piece);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-    }
 
-    // 체스판에 해당 기물이 몇 개 있는지 반환
-    public int countPieceFromBoard(Piece.Color color, Piece.Type type) {
-        int count = 0;
-        for (Rank rank : BOARD) {
-            count += rank.countPieceFromRank(color, type);
-        }
-        return count;
-    }
+//    // 위치 입력값 검증
+//    private void isValidNotation(String notation) {
+//        if (notation.length() != 2) throw new IllegalArgumentException("올바른 입력이 아닙니다.");
+//        char file = notation.charAt(0);
+//        char rank = notation.charAt(1);
+//        if (!Character.isLetter(file) || !Character.isLowerCase(file)) {
+//            throw new IllegalArgumentException("올바른 파일값이 아닙니다.");
+//        } else if (!Character.isDigit(rank)) {
+//            throw new IllegalArgumentException("올바른 랭크값이 아닙니다.");
+//        }
+//    }
 
-    // 입력받은 위치에 있는 기물 반환
-    public Piece findPiece(String notation) {
-        while (true) {
-            try {
-                isValidNotation(notation);
-                int fileIndex = converter.fileToIndex(notation.charAt(0));
-                int rankIndex = converter.rankToIndex(notation.charAt(1));
-                return BOARD.get(rankIndex).findPieceFromRank(fileIndex);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-    }
+//    // 색깔에 따른 점수 계산
+//    public double calculatePoint(Piece.Color color) {
+//        double answer = 0.0;
+//        Map<Integer, Boolean> pawnDuplicationMap = new HashMap<>();
+//
+//        for (int i = 0; i < FILE_SIZE; i++) {
+//            pawnDuplicationMap.put(i, isPawnDuplicatedInFile(color, i));
+//        }
+//
+//        for (int i = 0; i < FILE_SIZE; i++) {
+//            boolean pawnDuplicationCheck = pawnDuplicationMap.get(i);    // 새로줄 내에 폰 중복 여부
+//            for (int j = 0; j < RANK_SIZE; j++) {
+//                Piece piece = BOARD.get(j).findPieceFromRank(i);
+//                if (piece.getColor() == color) {
+//                    if (piece.getType() == Piece.Type.PAWN && pawnDuplicationCheck) {
+//                        answer += piece.getType().getDefaultPoint() / 2.0;
+//                    } else {
+//                        answer += piece.getType().getDefaultPoint();
+//                    }
+//                }
+//            }
+//        }
+//        return answer;
+//    }
 
-    // 위치 입력값 검증
-    private void isValidNotation(String notation) {
-        if (notation.length() != 2) throw new IllegalArgumentException("올바른 입력이 아닙니다.");
-        char file = notation.charAt(0);
-        char rank = notation.charAt(1);
-        if (!Character.isLetter(file) || !Character.isLowerCase(file)) {
-            throw new IllegalArgumentException("올바른 파일값이 아닙니다.");
-        } else if (!Character.isDigit(rank)) {
-            throw new IllegalArgumentException("올바른 랭크값이 아닙니다.");
-        }
-    }
-
-    // 색깔에 따른 점수 계산
-    public double calculatePoint(Piece.Color color) {
-        double answer = 0.0;
-        Map<Integer, Boolean> pawnDuplicationMap = new HashMap<>();
-
-        for (int i = 0; i < FILE_SIZE; i++) {
-            pawnDuplicationMap.put(i, isPawnDuplicatedInFile(color, i));
-        }
-
-        for (int i = 0; i < FILE_SIZE; i++) {
-            boolean pawnDuplicationCheck = pawnDuplicationMap.get(i);    // 새로줄 내에 폰 중복 여부
-            for (int j = 0; j < RANK_SIZE; j++) {
-                Piece piece = BOARD.get(j).findPieceFromRank(i);
-                if (piece.getColor() == color) {
-                    if (piece.getType() == Piece.Type.PAWN && pawnDuplicationCheck) {
-                        answer += piece.getType().getDefaultPoint() / 2.0;
-                    } else {
-                        answer += piece.getType().getDefaultPoint();
-                    }
-                }
-            }
-        }
-        return answer;
-    }
-
-    // 세로줄 내에 같은 색 폰이 2개 이상 있으면 true
-    private boolean isPawnDuplicatedInFile(Piece.Color color, int fileIndex) {
-        int count = 0;
-        for (Rank rank : BOARD) {
-            Piece piece = rank.findPieceFromRank(fileIndex);
-            if (piece.getColor() == color && piece.getType() == Piece.Type.PAWN) {
-                count++;
-                if (count >= 2) return true;
-            }
-        }
-        return false;
-    }
+//    // 세로줄 내에 같은 색 폰이 2개 이상 있으면 true
+//    private boolean isPawnDuplicatedInFile(Piece.Color color, int fileIndex) {
+//        int count = 0;
+//        for (Rank rank : BOARD) {
+//            Piece piece = rank.findPieceFromRank(fileIndex);
+//            if (piece.getColor() == color && piece.getType() == Piece.Type.PAWN) {
+//                count++;
+//                if (count >= 2) return true;
+//            }
+//        }
+//        return false;
+//    }
 
     public int getInitialPieceTotalCount() {
         return pieceCount;
