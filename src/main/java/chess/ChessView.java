fc856e44d44aa8fb40312811f3fd6299f73b879d
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-14 12:53:05 +0900

    feat: ChessView 클래스 수정
    현재 턴, 이동 성공, 이동 실패 출력 기능 구현

--- src/main/java/chess/ChessView.java
+++ src/main/java/chess/ChessView.java
@@ -1,5 +1,7 @@
 package chess;
 
+import pieces.Piece;
+
 import java.util.List;
 
 import static utils.StringUtils.appendNewLine;
@@ -8,8 +10,12 @@ public class ChessView {    // 출력 담당
 
     private final int RANK_SIZE = 8;
 
+    public void showTurn(String turn) {
+        System.out.println(turn + "의 차례입니다.");
+    }
+
     // 출력할 체스판 구성
-    public String showBoard(List<Rank> chessBoard) {
+    private String showBoard(List<Rank> chessBoard) {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < RANK_SIZE; i++) {
             builder.append(chessBoard.get(i).getRankView()).append("    ").append(appendNewLine(String.valueOf(RANK_SIZE - i)));
@@ -20,5 +26,17 @@ public class ChessView {    // 출력 담당
 
     public void printBoard(List<Rank> board) {
         System.out.println(showBoard(board));
+    }
+
+    public void printMoveSuccess(Piece currentPiece, Position sourcePosition, Position targetPosition) {
+        String source = sourcePosition.getNotation();
+        String target = targetPosition.getNotation();
+        System.out.println(currentPiece.getType() + "을 " + source + "에서 " + target + "으로 이동하는데 성공했습니다.");
+    }
+
+    public void printMoveFailure(Piece currentPiece, Position sourcePosition, Position targetPosition) {
+        String source = sourcePosition.getNotation();
+        String target = targetPosition.getNotation();
+        System.out.println(currentPiece.getType() + "을 " + source + "에서 " + target + "으로 이동하는데 실패했습니다.");
     }
 }
